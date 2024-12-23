ed228572b41d2ed3abc146df97cd9e9870a17a62
Ajay Saini <ajaysaini0505@gmail.com>
2024-12-23 15:51:15 +0530

    changed logo

---
+++ moboMart-Client/mobomart-v1/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/Navbar.css';
+import logo from "../assets/android-chrome-192x192.png";
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <Link to="/">
+                <img src={logo} alt="MoboMart Logo" className="navbar-logo" />
+                </Link>
+            </div>
+            <div className="search-bar">
+                <input type="text" placeholder="Search..." />
+            </div>
+            <div className="auth-buttons">
+                <Link to="/login" className="auth-button">Login</Link>
+                <Link to="/signup" className="auth-button">Sign Up</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
