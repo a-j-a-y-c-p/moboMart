e2fd5ddca5931941d4be9ef2e4814646fa419ff6
Ajay Saini <ajaysaini0505@gmail.com>
2024-12-24 18:00:17 +0530

    navbar changes

--- moboMart-Client/mobomart-v1/src/components/Navbar.js
+++ moboMart-Client/mobomart-v1/src/components/Navbar.js
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 import logo from "../assets/android-chrome-192x192.png";
 
-
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="logo">
                 <Link to="/">
-                <img src={logo} alt="MoboMart Logo" className="navbar-logo" />
+                    <img src={logo} alt="MoboMart Logo" className="navbar-logo" />
                 </Link>
+            </div>
+            <div className="left-links">
+                <Link to="/" className="navbar-link">Home</Link>
+                <Link to="/old-smartphone" className="navbar-link">Old Smartphone</Link>
+                <Link to="/new-smartphone" className="navbar-link">New Smartphone</Link>
             </div>
             <div className="search-bar">
                 <input type="text" placeholder="Search..." />
